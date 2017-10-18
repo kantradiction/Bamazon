8ed4094ddd180be8449fcba3544e17fc98220fc1
Andrew Presnell <andrew_presnell@hotmail.com>
2017-10-18 09:39:58 -0700

    Added Callback function - calling buy items immediately after read

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,24 +24,33 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
 	if (err) throw err;
 	console.log("connected as id " + connection.threadId);
+
+	//Immediately display items available for sale via afterConnection function
 	afterConnection();
-	connection.end();	
 });
+
+// Prompts
+// =============================================================
 
 function afterConnection() {
 	console.log("WE ARE NOW CONNECTED, SCOTTY");
-	read();
+
+	//Show items available for sale
+	read(buyItems);
+	//Prompt two questions: 
+	//1: Which id of the product would they like to buy
+	//2: How many units of the product would they like to buy
 };
 
-// CRUD Functions - ReST
+// CRUD Functions - ReST - End
 // =============================================================
 
 function create() {
-
+	console.log("CREATE FUNCTION THROWN");
 }
 
 //Display all of the items available for sale, including ids, names, and prices of the products for sale
-function read() {
+function read(arg) {
 	console.log("READ FUNCTION THROWN");
 	connection.query("SELECT * FROM products", function(err, res) {
 		if (err) throw err;
@@ -51,20 +60,55 @@ function read() {
 			console.log("\nID: " + res[i].item_id);
 			console.log("Name: " + res[i].product_name);
 			console.log("Department: " + res[i].department_name);
-			console.log("Price: " + res[i].price);
+			console.log("Price: $" + res[i].price);
 			console.log("Quantity: " + res[i].stock_quantity);
 		};
 
+		if (res) {
+			arg();
+		}
+
 		//to see what fields are in the response, uncomment out the next line
 		/*console.log(res);*/
-		connection.end();
+		/*connection.end();*/
 	})
 }
 
 function update() {
+	console.log("UPDATE FUNCTION THROWN");
 
 }
 
 function deleted() {
+	console.log("DELETED FUNCTION THROWN");
 
+}
+
+function end() {
+	console.log("ENDING CONNECTION");
+	connection.end();
+}
+
+function buyItems() {
+	inquirer.prompt([
+		{
+			type: "input",
+			name: "id",
+			message: "What is the ID of the product you would like to buy?"
+		},
+		{
+			type: "input",
+			name: "quantity",
+			message: "How many units of this product would you like to buy?"
+		}
+	]).then(function(response) {
+
+		//Complete the order
+		//Check if the store has enough of the product to meet the customer's request
+		//If false, the app should phrase "insufficient quantity!" and then prevent the order from going through
+		//If true, the app should fulfill the order and update the sequel database, then show the customer
+		// the total cost of their purchase
+		console.log(response);
+
+	})
 }
